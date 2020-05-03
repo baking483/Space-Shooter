8752e23cb86e183e85440c47b43fea3cc6536f86
baking483 <64713701+baking483@users.noreply.github.com>
2020-05-03 13:00:21 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var ball,img,paddle;
+function preload() {
+  ballimg = loadImage('ball.png');
+  paddleimg=loadImage("paddle.png")
+}
+function setup() {
+  createCanvas(400, 400);
+  ball=createSprite(60,200,20,20);
+  ball.addImage (ballimg); 
+  ball.velocityX = 2;  
+  ball.velocityY = Math.round(random(-8,8));
+  paddle=createSprite(350,200,20,100);
+  paddle.addImage(paddleimg)
+}
+
+function draw() {
+  background(205,153,0);
+  
+  edges=createEdgeSprites();
+  //Bounce Off the Left Edge only
+  
+  ball.bounceOff(edges[0]); 
+  ball.bounceOff(edges[2]);
+  ball.bounceOff(edges[3]);
+  
+  paddle.collide(edges);
+  
+  ball.bounceOff(paddle);
+  
+  if(keyDown(UP_ARROW))
+  {
+    paddle.y = paddle.y - 20;
+  }
+  
+  if(keyDown(DOWN_ARROW))
+  {
+    paddle.y =paddle.y + 20;
+  }
+  drawSprites();
+  
+}
+
+function explosion()
+{
+  ball.velocityY = Math.round(random(-8,8));
+}
+
